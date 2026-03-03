bb64f95d54a1d3b586cfd6e9b13467da1b2c90cd
Rafid43-code <rafidmohamed.4321@gmail.com>
2026-03-03 06:46:06 +0000

    first commit

---
+++ project1.py
@@ -0,0 +1,69 @@
+student_grade = {}
+
+
+
+# add a new student
+def add_student(name,grade):
+    student_grade[name] = grade
+    print(f"Added {name} with a mark {grade}")
+
+#update a student
+def update_student(name, grade):
+    if name in student_grade:
+       student_grade[name] = grade
+       print(f"updated {name} with a mark {grade} ")
+
+    else:
+        print(f"{name} is not found")
+#delete a student
+def delete_student(name):
+    if name in student_grade:
+        del student_grade[name]
+        print(f"{name} has been successfully deleted")
+    else:
+        print(f"{name} is not found!") 
+
+
+#view all student
+def display_all_student():
+    if student_grade:
+        for name,grade in student_grade.items():
+            print(f"{name} : {grade}")
+    else:
+         print("no student found/added")
+
+
+
+def main():
+    while True:
+        print("\n Student Grdes Management System")
+        print("1. Add Student")
+        print("2. update Student")
+        print("3. delete Student")
+        print("4. view Student")
+        print("5.  exit ")
+
+        choice = int(input("Enter your choice = "))
+        if choice == 1:
+            name = input("Enter student name = ")
+            grade = int(input("Enter student grade = "))
+            add_student(name,grade)
+
+        elif choice == 2:
+            name = input("Enter student name = ")
+            grade = int(input("Enter student grade = "))
+            update_student(name,grade)
+        
+        elif choice == 3:
+             name = input("Enter student name = ")
+             grade = int(input("Enter student grade = "))
+             delete_student(name)
+        elif choice == 4:
+             display_all_student()
+        elif choice == 5:
+            print("closing the program....")
+            break
+
+        else:
+            print("Invalid choice")
+main()
